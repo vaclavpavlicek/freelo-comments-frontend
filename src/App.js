57b44f9f0c952d8da4bc4955c0fa070ba4f56bf4
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-03-26 18:17:01 +0100

    Add loading data from JSON API.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import React, {Component} from 'react';
 import {Toolbar} from './components/Toolbar/Toolbar';
 import {Task} from './components/Task/Task';
-import avatar1 from './images/avatar1.png';
-import avatar2 from './images/avatar2.png';
-import avatar3 from './images/avatar3.png';
 import './App.scss';
 import {assocPath, append, pipe, takeLast, nth, inc, prop} from 'ramda';
+import * as axios from "axios";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            task: {
-                name: 'Merchandising stánku snědá tíseň na veletrhu v Mostě',
-                resolved: false,
-                dueDate: new Date('03-25-2019'),
-                assignee: {
-                    nickname: 'Dáša',
-                    profilePicture: avatar3
-                }
-            },
-            comments: [
-                {
-                    id: 1,
-                    nickname: 'Dáša',
-                    avatar: avatar3,
-                    text: '<p>Už mám první vlaštovku merche:</p><p><img src="./images/discussion1.png" className="img-fluid shadow-sm" alt=""/></p><p>Co ty na to brácha? <strong>@Ludan</strong></p>'
-                },
-                {
-                    id: 2,
-                    nickname: 'Luďan',
-                    avatar: avatar1,
-                    text: '<p>Už mám první vlaštovku merche:</p><p><img src="./images/discussion1.png" className="img-fluid shadow-sm" alt=""/></p><p>Co ty na to brácha? <strong>@Ludan</strong></p>'
-                },
-                {
-                    id: 3,
-                    nickname: 'Čočkin',
-                    avatar: avatar2,
-                    text: '<p>Hele a myslíte, že to bude fungovat?</p><p>Třeba já bych si na sebe takovou blbost nepřipnul. Neuděláme něco víc fancy? Třeba šátek?</p><p>Nebo mě ještě napadlo, že bychom mohli vyrábět brejle. Mrkejte:</p><p><img src=./images/discussion2.jpg width="400" className="img-fluid shadow-sm" alt=""/> </p>'
-                },
-                {
-                    id: 4,
-                    nickname: 'Luďan',
-                    avatar: avatar1,
-                    text: '<p>Hele <strong>@Cockin</strong> to mi nepřijde jako nejlepší nápad na to bych se vykašlal.</p>'
-                },
-            ]
-        };
+        this.state = {};
+        this.fetch();
     }
 
     addComment(text) {
@@ -54,10 +17,18 @@ class App extends Component {
             comments: append({
                 id: this.getId(),
                 nickname: 'Dáša',
-                avatar: avatar3,
+                avatar: './images/avatar3.png',
                 text,
             }, this.state.comments)
         });
+    }
+
+    fetch() {
+        axios.get('http://localhost:8000/rest/task-detail/1')
+            .then(response => {
+                this.setState(assocPath(['task', 'dueDate'], new Date(response.data.task.dueDate), response.data));
+            })
+            .catch(console.error);
     }
 
     getId() {
@@ -74,19 +45,33 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <div className="container container--w1440">
-                <div className="d-lg-flex my-4 my-md-5">
-                    <div className="flex-fill w-75">
-                        <Toolbar/>
-                        <Task task={this.state.task}
-                              comments={this.state.comments}
-                              addComment={this.addComment.bind(this)}
-                              updateTaskResolved={this.updateTaskResolved.bind(this)}/>
+        console.log(this.state);
+        if (this.state.task) {
+            return (
+                <div className="container container--w1440">
+                    <div className="d-lg-flex my-4 my-md-5">
+                        <div className="flex-fill w-75">
+                            <Toolbar/>
+                            <Task task={this.state.task}
+                                  comments={this.state.comments}
+                                  addComment={this.addComment.bind(this)}
+                                  updateTaskResolved={this.updateTaskResolved.bind(this)}/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div className="container container--w1440">
+                    <div className="d-lg-flex my-4 my-md-5">
+                        <div className="flex-fill w-75">
+                            <Toolbar/>
+                            <h1>Loading</h1>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
